b184346ae85581257de4ab8ec68575fc21cfa016
Colstin Donaldson <colstin50@gmail.com>
2023-04-07 12:04:51 -0700

    Initial Commit

---
+++ LearningApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LearningApp
+//
+//  Created by Colstin Donaldson on 4/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Colstin Donaldson on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
