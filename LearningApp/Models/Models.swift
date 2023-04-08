52b4aeb3820e1f02baca6a3e47edf894c6b3dd42
Colstin Donaldson <colstin50@gmail.com>
2023-04-07 17:15:52 -0700

    Add assets add data file add models parse json and style

---
+++ LearningApp/Models/Models.swift
@@ -0,0 +1,60 @@
+//
+//  Models.swift
+//  LearningApp
+//
+//  Created by Colstin Donaldson on 4/7/23.
+//
+
+import Foundation
+
+struct Module: Decodable, Identifiable{
+    
+    var id:Int
+    var category:String
+    var content:Content
+    var test:Test
+}
+
+struct Content: Decodable, Identifiable{
+    
+    var id:Int
+    var image:String
+    var time:String
+    var description:String
+    var lessons:[Lesson]
+}
+
+
+struct Lesson: Decodable, Identifiable{
+    
+    var id:Int
+    var title:String
+    var video:String
+    var duration:String
+    var explanation:String
+}
+
+struct Test: Decodable, Identifiable{
+    
+    var id:Int
+    var image:String
+    var time:String
+    var description:String
+    var questions:[Question]
+}
+
+struct Question: Decodable, Identifiable{
+    
+    var id:Int
+    var content:String
+    var correctIndex:Int
+    var answers:[String]
+}
+
+
+/*
+ 1. we use this [Question]  and not [String] because its an array of an object which is shown by the [] with {} inside of it plus it has an ID so we know it is gonna be its own thing. Basically its not just strings.
+ 
+ 2. On the very first one we use test:Test because test is not an array we are holding an entire object of a test shown by just using {}
+ 
+ */
--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -9,4 +9,46 @@ import Foundation
 
 class ContentModel: ObservableObject{
     
+    @Published var modules = [Module]()
+    
+    var styleData:Data?
+    
+    
+    init() {
+        getLocalData()
+    }
+    
+    func getLocalData(){
+        //1. Get a URL/ file path to json file .. Bundle.main.url(forResource: "Data", withExtension: "json")
+        //2. Read file into a data object...  Data(contentsOf: url)
+        //3. Create a JSONDecoder()
+        //4. Try to Decode json into an array or whatever... jsonDecoder.decode([Module].self, from: jsonData)
+        //5. assign parsed data into your published variables
+        //6. OPIONAL: set unique ID's for each instance ( IF NEEDED)
+        
+        let jsonUrl = Bundle.main.url(forResource: "Data", withExtension: "json") // solves the nil part
+        
+        do{
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            
+            let jsonDecoder = JSONDecoder()
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData) //decode it into an array of [Module]
+            
+            self.modules = modules
+            
+        } catch{
+            print(error)
+        }
+        // parse the css file
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do{
+            let styleData = try Data(contentsOf: styleUrl!)
+            
+            self.styleData = styleData
+            
+        } catch{
+            print(error)
+        }
+    }
 }
