44c80f879950b0eabe37e7d6e7bb0a5afe5d05e8
Colstin Donaldson <colstin50@gmail.com>
2023-04-07 18:16:52 -0700

    Modify HomeView to create part of the list of items Add the HomeViewRow to get rid of redundant code

--- LearningApp/Views/HomeView.swift
+++ LearningApp/Views/HomeView.swift
@@ -12,13 +12,33 @@ struct HomeView: View {
     @EnvironmentObject var model:ContentModel
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+       
+        
+        
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("What do you want to do today?")
+                    .padding(.leading)
+                
+                ScrollView{
+                    LazyVStack{
+                        ForEach(model.modules){ module in
+                            //learning card
+                            
+                            VStack(spacing: 20) {
+                                HomeViewRow(image: module.content.image, title: "Learn \(module.category)", description: module.content.description, count: "\(module.content.lessons.count) lessons", time: module.content.time)
+                                
+                                // test card
+                                HomeViewRow(image: module.test.image, title: "Learn \(module.category) Test", description: module.test.description, count: "\(module.test.questions.count) lessons", time: module.test.time)
+                            }
+                        }
+                    }
+                    .padding()
+                }
+            }
+            .navigationTitle("Get Started")
         }
-        .padding()
+        
     }
 }
 
---
+++ LearningApp/Views/HomeViewRow.swift
@@ -0,0 +1,66 @@
+//
+//  HomeViewRow.swift
+//  LearningApp
+//
+//  Created by Colstin Donaldson on 4/7/23.
+//
+
+import SwiftUI
+
+struct HomeViewRow: View {
+    
+    var image:String
+    var title:String
+    var description:String
+    var count:String
+    var time:String
+    
+    var body: some View {
+        ZStack{
+            Rectangle()
+                .foregroundColor(Color.white)
+                .cornerRadius(30)
+                .shadow(radius: 5)
+                .aspectRatio(CGSize(width: 335, height: 175), contentMode: .fit)
+            HStack{
+                Image(image)
+                    .resizable()
+                    .frame(width: 116, height: 116)
+                    .clipShape(Circle())
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 10){
+                    Text(title)
+                        .bold()
+                    Text(description)
+                        .font(.caption)
+                        .padding(.bottom, 20)
+     
+                    HStack{
+                        Image(systemName: "text.book.closed")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        Text(count)
+                            .font(Font.system(size: 12))
+                        
+                        Spacer()
+                        
+                        Image(systemName: "clock")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        Text(time)
+                            .font(Font.system(size: 12))
+                    }
+                }
+                .padding(.leading)
+            }
+            .padding(.horizontal)
+        }
+    }
+}
+
+struct HomeViewRow_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewRow(image: "swift", title: "Learn swift", description: "description", count: "20 lesson", time: "20 hours")
+    }
+}
