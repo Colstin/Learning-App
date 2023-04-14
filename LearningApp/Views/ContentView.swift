4e2185f751e0c89d814612101eac4c7e65a31eba
Colstin Donaldson <colstin50@gmail.com>
2023-04-14 12:46:42 -0700

    Refresh

--- LearningApp/Views/ContentView.swift
+++ LearningApp/Views/ContentView.swift
@@ -31,6 +31,7 @@ struct ContentView: View {
                         }
                     }
                 }
+                .padding()
             }
         }
         .foregroundColor(.black)
